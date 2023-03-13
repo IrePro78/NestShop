d70706894685afd69692de2c6e54ba2aabe9c90d
IrePro78 <gos3r78@gmail.com>
2023-03-13 15:00:21 +0100

    added TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopModule } from './shop/shop.module';
 import { BasketModule } from './basket/basket.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ShopModule, BasketModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'user',
+      password: 'user123',
+      database: 'shopNest',
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      bigNumberStrings: false,
+      logging: true,
+      synchronize: true,
+    }),
+    ShopModule,
+    BasketModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
